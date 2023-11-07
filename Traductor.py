daf3043b6d066727f5c8c9cce55c671a40c08c43
bayoneta101 <marcosgarciapostigo@gmail.com>
2023-11-07 21:39:23 +0100

    pruebas de la funcion autoTraducir

--- Traductor.py
+++ Traductor.py
@@ -1,24 +1,38 @@
 # Online Python - IDE, Editor, Compiler, Interpreter
 
-prueba={"Comun":['a', 'b', 'c','d',], "Arcano":['1','2','3','4']};
-
-#def test(x):
-#    print(x)    
-key_list = list(prueba.keys())    
-value_list = list(prueba.values())     
-print(key_list)
-print(value_list)
+alfabeto={"Comun":['a', 'b', 'c','d'],
+        "Arcano":['1','2','3','4'],
+        "Elfico":['5', '6', '7','8']}
 
 
-def idAlfabetoPorLetra(var):
-    pos = [i for (i, x) in enumerate(value_list) if var in x]
-    print(key_list[pos[0]])  
+key_list = list(alfabeto.keys())    
+value_list = list(alfabeto.values())  
+valueSingleList= [item for sublist in alfabeto.values() for item in sublist]
+
+
+
+def idAlfabetoPorLetra(letra):
+    pos = [i for (i, x) in enumerate(value_list) if letra in x]
+    
+    return key_list[pos[0]]
+
 
 def traducir(alfab1, alfab2, texto):
     texto_trad=""
     for letra in texto:
-        print(letra)
-    return
+        if letra in alfabeto[alfab1]:
+            texto_trad=texto_trad+alfabeto[alfab2][alfabeto[alfab1].index(letra)]
+        else:
+            texto_trad=texto_trad+letra
+    print(texto_trad)
 
-idAlfabetoPorLetra('1')
-traducir('Comun', 'Arcano', "abcd")
+
+def autoTraducir(alfab, texto):
+    for letra in texto:
+        if letra in valueSingleList:
+            return traducir(idAlfabetoPorLetra(letra), alfab, texto)
+    
+    
+#print(idAlfabetoPorLetra('a'))
+#traducir('Comun', 'Arcano', "abcd a")
+autoTraducir('Arcano',"abcd a")
